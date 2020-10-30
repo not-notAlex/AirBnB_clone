62b1a1e203b77b76cc58ef3fec6839c4394fd016
not-notAlex <alex.smith1815@gmail.com>
2020-10-30 23:55:58 +0000

    created models and engine files

---
+++ models/base_model.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+The base class to be inherited from
+"""
+
+
+from datetime import datetime
+from uuid import uuid4
+import models
+
+
+class BaseModel():
+    """
+    The base model
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        constructor
+        """
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.created_at = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "__class__":
+                    pass
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """
+        prints: [<class name>] (<self.id>) <self.__dict__>
+        """
+        output = "[" + self.__class__.__name__ + "] ("
+        output += str(self.id) + ") " + str(self.__dict__)
+        return output
+
+    def save(self):
+        """
+        updates the updated time
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        returns dictionary containing all key/values
+        """
+        new_dict = {}
+        new_dict.update({"__class__": self.__class__.__name__})
+        for key, val in self.__dict__.items():
+            if isinstance(val, datetime):
+                new_dict.update({key: val.isoformat()})
+            else:
+                new_dict.update({key: val})
+        return new_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+serializes instances from a file
+"""
+
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    creates and loads json files
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """
+        returns all objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        creates a new object
+        """
+        if obj:
+            o = obj.__class__.__name__ + "." + obj.id
+            self.__objects.update({o: obj})
+
+    def save(self):
+        """
+        saves objects to file
+        """
+        new_dict = {}
+        for key, val in self.__objects.items():
+            new_dict.update({key: val.to_dict()})
+        with open(self.__file_path, 'w') as f:
+            json.dump(new_dict, f)
+
+    def reload(self):
+        """
+        loads objects from file
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                old_objects = json.load(f)
+            for key, val in old_objects.items():
+                for k, v in val.items():
+                    if v == 'BaseModel':
+                        obj = BaseModel(**val)
+                self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
